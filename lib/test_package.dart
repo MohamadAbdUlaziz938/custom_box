5c4f74a539d3b5dd2dd2425120f048163c3ceb27
mohamad abdUlaziz <95186447+oshaWork@users.noreply.github.com>
2022-05-31 07:15:15 +0300

    v2

--- lib/test_package.dart
+++ lib/test_package.dart
@@ -1,7 +1,35 @@
 library test_package;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+
+import 'package:flutter/material.dart';
+
+class CustomAlertBox {
+  static Future showCustomAlertBox({
+    required BuildContext context,
+    required Widget willDisplayWidget,
+  }) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape:const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                willDisplayWidget,
+                MaterialButton(
+                  color: Colors.white30,
+                  child: Text('close alert'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+            elevation: 10,
+          );
+        });
+  }
 }
